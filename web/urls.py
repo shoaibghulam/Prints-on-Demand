0f44187926cfe91217453c4e4c00aab0a7680302
Shoaib Ghulam <shoaibghulam@gmail.com>
2023-07-27 02:56:43 +0500

    update about page and create login and singup and add dropdown when login

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('about',About.as_view(),name="about"),
     path('contact',Contact.as_view(),name="contact"),
     path('faq',Faq.as_view(),name="faq"),
+    path('login',Login.as_view(),name="login"),
+    path('signup',Signup.as_view(),name="signup "),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.views import View
 # Create your views here.
 class Home(View):
@@ -18,3 +18,16 @@ class Contact(View):
 class Faq(View):
     def get(self, request):
         return render(request,"public/faq.html")
+
+class Login(View):
+    def get(self, request):
+       
+        return render(request,"public/login.html")
+    def post(self, request):
+         request.session["login"] =True
+         return redirect("/")
+
+
+class Signup(View):
+    def get(self, request):
+        return render(request,"public/signup.html")
