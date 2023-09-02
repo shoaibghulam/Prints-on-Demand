df56ace9ada901c0fe63ab5c2d40da0395e1f9d2
Shoaib Ghulam <shoaibghulam@gmail.com>
2023-09-03 00:26:41 +0500

    new changes

--- core/settings.py
+++ core/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shoaibghulam32@gmail.com'
-EMAIL_HOST_PASSWORD = 'shoaibghulambaloch'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
