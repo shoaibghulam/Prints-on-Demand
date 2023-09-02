f617fca27a2c2528a833ef10d4b188e62f2c4d68
Shoaib Ghulam <shoaibghulam@gmail.com>
2023-09-03 00:27:23 +0500

    new changes add backend

--- web/models.py
+++ web/models.py
@@ -18,7 +18,7 @@ class UserModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    is_email_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=True)
     email_verification_token = models.CharField(max_length=255, blank=True, null=True) 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
